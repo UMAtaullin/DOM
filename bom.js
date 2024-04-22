60d8d923b414ec63acfef7b8d5dfec4d48289d32
Ural Ataullin <ataullinural@gmail.com>
2024-04-22 13:50:40 +0500

    dom

--- bom.js
+++ bom.js
@@ -5,6 +5,6 @@ alert('hi')
 const confirmResult = confirm('Хочешь верстать?');
 console.log(confirmResult)
 
-//
+// Отмена - null, ничего - пустая строка
 const promptResult = prompt('Who are you in life?')
 console.log(promptResult)
