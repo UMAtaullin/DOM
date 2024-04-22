35dface57e0c21423094029724d873c2c37d485b
Ural Ataullin <ataullinural@gmail.com>
2024-04-22 13:11:11 +0500

    Initial commit

---
+++ script1.js
@@ -0,0 +1,17 @@
+// WINDOW
+const windowWidth = window.innerWidth;
+console.log(`Ширина окна браузера: ${windowWidth}px`)
+
+// О браузере о ОС
+console.log(navigator.userAgent);
+
+// Текущий адрес страницы
+console.log(location.href);
+
+// Перенаправить пользователя
+// location.href='https://fls.guru';
+
+// История браузера и передвигаться по страницам
+// history.back();
+// history.forward();
+
---
+++ script2.js
@@ -0,0 +1,10 @@
+// Простое окно с текстом
+alert('hi')
+
+// Уже две кнопки
+const confirmResult = confirm('Хочешь верстать?');
+console.log(confirmResult)
+
+//
+const promptResult = prompt('Who are you in life?')
+console.log(promptResult)
